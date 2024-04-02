a758d011ca4c612c722d6b204d8ff4ff86d9ca02
longlinh123456 <nope@example.com>
2024-04-02 18:37:09 +0700

    added batch user get api

--- src/apis/users.rs
+++ src/apis/users.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_iter::CloneOnce;
 
 use crate::{AuthenticatedClient, BaseClient, RequestResult};
 
@@ -11,6 +12,21 @@ pub struct AuthenticatedUser {
     pub id: Id,
     pub name: String,
     pub display_name: String,
+}
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct BatchUserInfoRequest<T: Iterator<Item = Id>> {
+    #[serde(with = "serde_iter::seq")]
+    user_ids: CloneOnce<Id, T>,
+    exclude_banned_users: bool,
+}
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct BatchUser {
+    pub id: Id,
+    pub name: String,
+    pub display_name: String,
+    pub has_verified_badge: bool,
 }
 macro_rules! add_base_url {
     ($api_route: literal) => {
@@ -33,3 +49,26 @@ pub trait UsersAuthenticatedApi: AuthenticatedClient {
 }
 
 impl<T: AuthenticatedClient> UsersAuthenticatedApi for T {}
+
+#[async_trait]
+pub trait UsersApi: BaseClient {
+    /// Limit of 200 users/request
+    async fn get_user_info_batch<T>(
+        &self,
+        users: T,
+        exclude_banned_users: bool,
+    ) -> RequestResult<BatchUser>
+    where
+        T: IntoIterator<Item = Id> + Send,
+        T::IntoIter: Send,
+    {
+        self.post::<BatchUser, BatchUserInfoRequest<T::IntoIter>>(
+            add_base_url!("v1/users"),
+            BatchUserInfoRequest {
+                user_ids: users.into_iter().into(),
+                exclude_banned_users,
+            },
+        )
+        .await
+    }
+}
